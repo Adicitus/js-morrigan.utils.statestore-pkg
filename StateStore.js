348acfbb8f6036f1f57a45ceb8602ffa37f72e91
Joakim Olsson <joakim.olsson@adicitus.cloud>
2022-09-22 01:07:47 +0200

    Corrected tests and fixed API errors.

--- StateStore.js
+++ StateStore.js
@@ -18,6 +18,15 @@ const mode = {
     memory: 'memory'
 }
 
+const namespaceFormat = /^[a-z0-9-_]+$/i
+
+var rootStore = null
+
+/**
+ * Regular expression for valid namespace format.
+ */
+module.exports.VALID_NAMESPACE_FORMAT = namespaceFormat
+
 /**
  * Simple scope, only allows getting, setting and removing of data in this store.
  */
@@ -43,16 +52,14 @@ module.exports.MODE_TEST        = 'test'
 /**
  * Generates a new state store API object that can be used to persist data.
  * @param {string} parentPath Path to the folder where the store should be created. 
- * @param {string} namespace Name for this store and any subordinate stores.
+ * @param {string} namespace Name for this store and. This will be profixed to the name of any subordinate stores. Should match regex /^[a-z0-9\-_]$/i
  * @param {object} options Optional settings. CUrrently only 'scope' is recognized.
  * @returns This store object.
  */
 function StateStore(parentPath, namespace, options) {
 
-    const self = this
-
     if (!moduleState.initialized) {
-        throw "Uninitialized"
+        throw "Uninitialized: Global StateStore settings have not been applied. Please run the exported .setup function to initialize the module."
     }
 
     const { Low, JSONFile, Memory } = moduleState.lowdb
@@ -76,49 +83,53 @@ function StateStore(parentPath, namespace, options) {
     
     db.read()
 
-    //console.log(db)
-
     this.getNamespace = () => {
         return namespace
     }
 
     this.set = async (name, value) => {
         if (db.data === null) {
-            db.data = {}
+            await db.read()
         }
 
         db.data[name] = value
-        db.write()
+        await db.write()
     }
 
     this.get = async (name) => {
+        await db.read()
         return db.data[name]
     }
 
     this.remove = async (name) => {
         delete db.data[name]
-        db.write()
+        await db.write()
     }
 
     if (options) {
-        //console.log(options)
-        //console.log((`${scopes[options.scope]} > ${scopes[state.scope]}`))
         if (options.scope) {
             state.scope = (scopes[options.scope] > scopes[state.scope])? options.scope : state.scope 
         }
     }
 
+    const parent = this
+
+    let createStore = async (namespace, scope) => {
+
+        if (!namespaceFormat.test(namespace)) {
+            throw `Invalid name namespace provided (should only contain characters a-z, 0-9, - and _): ${namespace}`
+        }
+
+        return new StateStore(storePath, `${parent.getNamespace()}.${namespace}`, { scope })
+    }
+
     switch(scopes[state.scope]) {
         case scopes.full:
             this.db = db
-            this.getStore = async (namespace, scope) => {
-                return new StateStore(storePath, namespace, { scope })
-            }
+            this.getStore = createStore
             break
         case scopes.delegate:
-            this.getStore = async (namespace, scope) => {
-                return new StateStore(storePath, namespace, { scope })
-            }
+            this.getStore = createStore
             break
         case scopes.simple:
         default:
@@ -187,12 +198,21 @@ module.exports.setup = async (rootPath, options) => {
     validateModuleState()
 
     moduleState.initialized = true
+
+    rootStore = new StateStore(moduleState.rootPath, 'global', { scope: module.exports.SCOPE_FULL })
+
+    return rootStore
+}
+
+/**
+ * Returns the root store object if one exists, otherwise returns null.
+ * 
+ * @returns The root store, if the setup method has been called. Otherwise returns null.
+ */
+module.exports.getRootStore = async () => {
+    return rootStore
 }
 
 module.exports.getStore = async (namespace, scope) => {
-    if (!moduleState.initialized) {
-        throw "Uninitialized"
-    }
-
-    return new StateStore(moduleState.rootPath, namespace, { scope }) 
+    return await rootStore.getStore(namespace, scope)
 }
--- test/test.js
+++ test/test.js
@@ -1,140 +1,155 @@
 const StateStore = require('../StateStore')
+const assert = require('assert')
 
 
 function determineScope(store) {
     if (store.db) { return 'full' }
     if (store.getStore) {return 'delegate'}
-    return 'limited'
+    return 'simple'
 }
 
 describe('StateStore', () => {
 
+    var rootStore = null
+
     describe("Module", () => {
 
-        describe("getStore", () => {
+        describe('setup', () => {
 
-            before(async () => {
-                await StateStore.setup('C:\\morrigan.server\\dev\\state')
+            it("Should return a 'root' store when setup finishes.", async () => {
+                rootStore = await StateStore.setup('C:\\morrigan.server\\test\\state\\')
+                assert.deepEqual(rootStore.getNamespace(), 'global')
             })
+        })
 
-            it("Should create stores with scope 'simple' by default.", (done) => {
-                let p = StateStore.getStore('defaultTest').then((store) => {
-                    assert.ok(store)
-                    assert.deepEqual(determineScope(store), 'simple')
-                    console.log(p)
-                }).catch(err => {
-                    assert.fail(err)
-                }).finally(() => { done() })
-            })
+        describe('getRootStore', () => {
 
-            it("Should allow explicit creation of 'simple' stores.", (done) => {
-                StateStore.getStore('simpleTest', 'simple').then((store) => {
-                    assert.ok(store)
-                    assert.deepEqual(determineScope(store), 'simple')
-                }).catch(err => {
-                    assert.fail(err)
-                }).finally(() => { done() })
-            })
-            
-            it("Should allow explicit creation of 'delegate' stores.", (done) => {
-                StateStore.getStore('delegateTest', 'delegate').then((store) => {
-                    assert.ok(store)
-                    assert.deepEqual(determineScope(store), 'delegate')
-                }).catch(err => {
-                    assert.fail(err)
-                }).finally(() => { done() })
-            })
-
-            it("Should allow explicit creation of 'full' stores.", (done) => {
-                StateStore.getStore('fullTest', 'full').then((store) => {
-                    assert.ok(store)
-                    assert.deepEqual(determineScope(store), 'full')
-                    console.log(store)
-                }).catch(err => {
-                    assert.fail(err)
-                }).finally(() => { done() })
+            it("Should return the same store instance as 'setup'.", async () => {
+                let store = await StateStore.getRootStore()
+                assert.deepEqual(store, rootStore)
             })
         })
     })
 
     describe("Store", () => {
-
-        describe("getStore", () => {
-            var delegateStore = null
-
-            before(async () => {
-                await StateStore.setup('C:\\morrigan.server\\dev\\state')
-                delegateStore = await StateStore.getStore('delegate', 'delegate')
-            })
-
-            it("Should create stores with scope 'simple' by default.", (done) => {
-                delegateStore.getStore('defaultTest').then((store) => {
-                    assert.ok(store)
-                    assert.deepEqual(determineScope(store), 'simple')
-                }).catch(err => {
-                    assert.fail(err)
-                }).finally(done)
-            })
-
-            it("Should allow explicit creation of 'simple' stores.", (done) => {
-                delegateStore.getStore('simpleTest', { scope: 'simple' }).then((store) => {
-                    assert.ok(store)
-                    assert.deepEqual(determineScope(store), 'simple')
-                }).catch(err => {
-                    assert.fail(err)
-                }).finally(done)
-            })
-            
-            it("Should allow explicit creation of 'delegate' stores.", (done) => {
-                delegateStore.getStore('delegateTest', { scope: 'delegate' }).then((store) => {
-                    assert.ok(store)
-                    assert.deepEqual(determineScope(store), 'delegate')
-                }).catch(err => {
-                    assert.fail(err)
-                }).finally(done)
-            })
-
-            it("Should allow explicit creation of 'full' stores.", (done) => {
-                delegateStore.getStore('fullTest', { scope: 'full' }).then((store) => {
-                    assert.ok(store)
-                    assert.deepEqual(determineScope(store), 'full')
-                }).catch(err => {
-                    assert.fail(err)
-                }).finally(done)
-            })
-        })
         
         var store = null
         const testValue = Math.random().toString(16).split('.')[1]
 
         before(async () => {
-            store = await StateStore.getStore('storeTest')
+            store = await StateStore.getStore('storeTest', 'full')
         })
 
-        it("Should allow storage of data using the 'set' method.", (done) => {
-            store.set('test', testValue).catch(() => {
-                assert.fail()
-            }).finally(done)
+        it("Should allow asynchronous storage of data using the 'set' method.", async () => {
+            await store.set('test', testValue)
         })
 
-        it ("Should allow the retriveal of data using the 'get' method.", (done) => {
-            store.get('test').then(v => {
-                assert.deepEqual(v, testValue)
-            }).catch(err => {
-                assert.fail(err)
-            }).finally(done)
+        it ("Should allow asynchronous retriveal of data using the 'get' method.", async () => {
+            let v = await store.get('test')
+            assert.deepEqual(v, testValue)
         })
 
-        it ("Should allow the removal of data using the 'remove' method.", (done) => {
-            store.remove('test').then(() => {
-                store.get('test').then(v => {
-                    assert.deepEqual(v, null)
-                }).catch(err => {
-                    assert.fail(err)
-                }).finally(done)
-            }).catch(err => {
-                assert.fail(err)
-                done()
+        it ("Should allow asynchronous removal of data using the 'remove' method.", async () => {
+            await store.remove('test')
+            let v = await store.get('test')
+            assert.deepEqual(v, null)
+        })
+
+        
+        describe("getStore", () => {
+            var delegateStore = null
+
+            before(async () => {
+                delegateStore = await rootStore.getStore('delegate', 'delegate')
+            })
+
+            it("Should create stores with scope 'simple' by default.", async () => {
+                let store = await delegateStore.getStore('defaultTest')
+                assert.ok(store)
+                assert.deepEqual(determineScope(store), 'simple')
+            })
+
+            it("Should allow explicit creation of 'simple' stores.", async () => {
+                let store = await delegateStore.getStore('simpleTest', 'simple')
+                assert.ok(store)
+                assert.deepEqual(determineScope(store), 'simple')
+            })
+            
+            it("Should allow explicit creation of 'delegate' stores.", async () => {
+                let store = await delegateStore.getStore('delegateTest', 'delegate')
+                assert.ok(store)
+                assert.deepEqual(determineScope(store), 'delegate')
+            })
+
+            it("Should allow explicit creation of 'full' stores.", async () => {
+                let store = await delegateStore.getStore('fullTest', 'full')
+                assert.ok(store)
+                assert.deepEqual(determineScope(store), 'full')
+            })
+
+            describe("Namespaces", () => {
+                it("Namespace should accept characters a-Z, 0-9, -, _ (case-insesitive)", async () => {
+                    let store = await delegateStore.getStore('abcdefghijklmnopqrstuvvwxyzABCDEFGHIJKLMNOPQRSTUVVWXYZ0123456789-_')
+                    assert.ok(store)
+                    assert.deepEqual(determineScope(store), 'simple')
+                })
+
+                it("Namespace containng invalid characters should be rejected with an exception.", async () => {
+
+                    let namespaces = [
+                        'validNamespace',
+                        'valid-namespace',
+                        'valid_namespace',
+                        'invalid namespace', // Should fail
+                        '.MaliciousNamespace', // Should fail
+                        '', // Empty namespace, should fail
+                    ]
+
+                    for(var i in namespaces) {
+                        let namespace = namespaces[i]
+                        let store = null
+                        try {
+                            store = await delegateStore.getStore(namespace)
+                        } catch {
+                            // This is the intended behavior
+                        }
+
+                        if (!StateStore.VALID_NAMESPACE_FORMAT.test(namespace) && store) {
+                            assert.fail(`Invalid namespace name accepted: ${namespace}`)
+                        }
+                    }
+                })
+            })
+
+            describe("Namespace data", () => {
+
+                const accessTestValue = Math.random().toString(16).split('.')[1]
+                const persistTestValue = Math.random().toString(16).split('.')[1]
+
+                it(`Two stores with the same namespace should access the same data. (nonce: ${accessTestValue})`, async () => {
+                    let v1 = accessTestValue
+                    let store1 = await delegateStore.getStore('accessTest', 'full')
+                    let store2 = await delegateStore.getStore('accessTest', 'full')
+
+                    await store1.set('value', v1)
+                    let v2 = await store2.get('value')
+                    
+                    assert.equal(store1.getNamespace(), store2.getNamespace())
+                    assert.notEqual(store1.db, store2.db)
+                    assert.deepEqual(v1, v2)
+                })
+
+                it(`Namespace data should persist if the original store object is deleted. (nonce: ${persistTestValue})`, async () => {
+                    let v1 = persistTestValue
+                    let store1 = await delegateStore.getStore('accessTest', 'full')
+                    await store1.set('value', v1)
+                    delete store1
+
+                    
+                    let store2 = await delegateStore.getStore('accessTest', 'full')
+                    let v2 = await store2.get('value')
+                    assert.deepEqual(v1, v2)
+                })
             })
         })
     })
